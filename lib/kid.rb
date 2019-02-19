3ed516de102a89f0e55c73fdff8fb2ff8006627b
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-19 04:08:39 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+class Kid 
+  def initialize (name)
+    @name=name
+  end
+  attr_accessor :name
+ extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end 
