7d93951a3c01a6815a3c27cfcd38d89543e85875
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-19 04:05:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid 
-  extend MetaDancing
+ extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end 
